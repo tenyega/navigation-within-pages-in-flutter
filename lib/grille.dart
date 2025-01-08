8ac41bdaf84f67ac1a048c7a523ae5c14e058c6b
tenyega <tenyega23@gmail.com>
2025-01-08 12:29:39 +0100

    finally the grill with orientation in the terminal

--- lib/grille.dart
+++ lib/grille.dart
@@ -10,6 +10,7 @@ class Grille extends StatefulWidget {
 class GrilleState extends State<Grille> {
   var entries = <int>[1, 2, 3, 4, 5, 6];
   var colorCodes = <int>[100, 400, 700, 800, 900, 1000];
+  var Orientation;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,28 +20,43 @@ class GrilleState extends State<Grille> {
       ),
       body: Center(
         child: GridView.builder(
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            itemCount: entries.length,
-            itemBuilder: (BuildContext context, int index) {
-              return InkWell(
-                onTap: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => Container(
-                              height: 50,
-                              color: Colors.amber[colorCodes[index]],
-                              child: Center(
-                                  child: Card(
-                                      color: const Color.fromARGB(
-                                          255, 138, 173, 139),
-                                      child: Text('Entry ${entries[index]}'))),
-                            )),
-                  )
-                },
-              );
-            }),
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return InkWell(
+              onTap: () {
+                // Retrieve the current orientation
+                var orientation = MediaQuery.of(context).orientation;
+
+                // Print the orientation (optional)
+                print(orientation);
+
+                // Navigate to a new screen
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(title: Text("Detail Page")),
+                      body: Center(
+                        child: Container(
+                          height: 50,
+                          color: Colors.amber[colorCodes[index]],
+                          child: Card(
+                            color: const Color.fromARGB(255, 138, 173, 139),
+                            child: Center(
+                              child: Text('Entry ${entries[index]}'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
